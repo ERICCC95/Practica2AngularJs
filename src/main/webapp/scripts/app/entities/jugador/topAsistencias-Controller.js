a87f4e4a5972acb46326411bc5e31518033cd9c3
alfredoru <alfredoru@gmail.com>
2016-04-13 18:57:07 +0200

    inicio

---
+++ src/main/webapp/scripts/app/entities/jugador/topAsistencias-Controller.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('ligaBaloncestoApp')
+    .controller('TopAsistenciasController', function ($scope, $rootScope, $stateParams, entity, Jugador, Equipo, ParseLinks) {
+
+        entity.$promise.then(function(data){
+            console.log(data);
+            var link = entity.$httpHeaders('link');
+
+            $scope.links = ParseLinks.parse(link);
+
+            $scope.savedPlayers = data;
+        });
+
+
+
+
+
+    });
+
